8a81a4c21dbc67e2a64d20d26c429929411a78d6
LeonE2024 <leonelflein@gmail.com>
2024-10-28 17:01:42 +0100

    feat: 20241028

--- src/vpa_robot_vision/scripts/visual_detector.py
+++ src/vpa_robot_vision/scripts/visual_detector.py
@@ -174,7 +174,7 @@ class RobotVision:
         self.inter_boundary_line_hsv = HSVSpace( 50,  20, 240, 140, 220, 130)
 
         # guiding lines inside intersections - no dynamic reconfigure
-        self._right_guide_hsv = HSVSpace(140, 100, 120,  80, 250, 200)
+        self._right_guide_hsv = HSVSpace(125, 95, 140,  110, 240, 210)
         self._left_guide_hsv  = HSVSpace(160, 140, 180,  80, 230, 160)
         self._thur_guide_hsv  = HSVSpace( 30,   0, 250, 170, 230, 130)  
         self.inter_guide_line = [self._thur_guide_hsv, self._left_guide_hsv, self._right_guide_hsv]
@@ -212,6 +212,7 @@ class RobotVision:
 
         if self.test_mode:
             self.test_mode_func(cv_img=cv_img, cv_hsv_img=cv_hsv_img)
+            return
 
         """Step2 BOUNDARY LINE DETECTOR"""
         self.detect_inter_boundary_line(cv_hsv_img=cv_hsv_img) 
