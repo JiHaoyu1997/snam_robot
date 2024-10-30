d75c6032977f8d2adff410b8e5a8df5505c8fb00
LeonE2024 <leonelflein@gmail.com>
2024-10-30 22:24:34 +0100

    feat: 20241030

--- src/vpa_robot_vision/scripts/visual_detector.py
+++ src/vpa_robot_vision/scripts/visual_detector.py
@@ -334,7 +334,6 @@ class RobotVision:
     def detect_conflict_boundary_line(self, cv_hsv_img):
         dis2conflict = search_pattern.search_line(hsv_image=cv_hsv_img, hsv_space=self.stop_line_hsv)
         print(dis2conflict)
-        print(1)
         if dis2conflict > 30:
             self.enter_conflict_zone = True
     
@@ -349,7 +348,7 @@ class RobotVision:
 
     def get_target_to_cross_lane(self, cv_img, cv_hsv_img):
         target_x = search_pattern.search_lane_center(self.center_line_hsv, self.side_line_hsv, cv_hsv_img, is_yellow_left=True)
-        print(target_x)
+        print(f"target_x: {target_x}adhqlkjdhqdkqjwdl")
         if target_x == None:
             target_x = self.image_width / 2
         cv2.circle(cv_img, (int(target_x), int(cv_hsv_img.shape[0]/2)), 5, (0, 255, 0), 5)
