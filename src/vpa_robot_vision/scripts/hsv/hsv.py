88a7f87a4d38b3f4ced3e5ad619e0f6918bf8bb7
LeonE2024 <leonelflein@gmail.com>
2024-11-13 20:16:14 +0100

    config kernel

--- src/vpa_robot_vision/scripts/hsv/hsv.py
+++ src/vpa_robot_vision/scripts/hsv/hsv.py
@@ -25,14 +25,14 @@ class HSVSpace:
     
     def apply_mask(self, hsv_image):
         _mask   = cv2.inRange(hsv_image, self._generate_lower_mask(), self._generate_upper_mask())
-        _kernel = np.ones((9,9), np.uint8)
+        _kernel = np.ones((5,5), np.uint8)
         _mask   = cv2.morphologyEx(_mask,cv2.MORPH_CLOSE, _kernel)
         return _mask
 
 HSV_RANGES = {
     'white':    HSVSpace(h_u=179, h_l=0,   s_u=30,  s_l=0,   v_u=255, v_l=200),
     'red':      HSVSpace(h_u=180, h_l=170, s_u=220, s_l=180, v_u=255, v_l=100),  # 或者 h_u=180, h_l=170
-    'orange':   HSVSpace(h_u=30,  h_l=0,   s_u=255, s_l=100, v_u=235, v_l=180),
+    'orange':   HSVSpace(h_u=20,  h_l=10,   s_u=255, s_l=100, v_u=235, v_l=180),
     'yellow':   HSVSpace(h_u=35,  h_l=25,  s_u=255, s_l=100, v_u=255, v_l=100),
     'green':    HSVSpace(h_u=100, h_l=50,  s_u=255, s_l=100, v_u=255, v_l=100),
     'blue':     HSVSpace(h_u=125, h_l=95,  s_u=255, s_l=180, v_u=255, v_l=120),
