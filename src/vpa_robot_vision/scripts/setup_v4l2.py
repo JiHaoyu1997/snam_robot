e3b5aa63689f0bc4afb793e130293ec3df8d0007
LeonE2024 <leonelflein@gmail.com>
2024-11-17 17:31:58 +0100

    v4l2

--- src/vpa_robot_vision/scripts/setup_v4l2.py
+++ src/vpa_robot_vision/scripts/setup_v4l2.py
@@ -7,7 +7,7 @@ def setup_v4l2():
     commands = [
         "v4l2-ctl -d /dev/video0 --set-ctrl saturation=0",
         "v4l2-ctl -d /dev/video0 --set-ctrl contrast=0",
-        "v4l2-ctl -d /dev/video0 --set-ctrl white_balance_auto_preset=3",
+        "v4l2-ctl -d /dev/video0 --set-ctrl white_balance_auto_preset=1",
         "v4l2-ctl -d /dev/video0 --set-ctrl scene_mode=11",
     ]
 
