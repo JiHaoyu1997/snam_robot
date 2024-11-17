7966adb1bdfc46fa59aaa3c02af09ab2b5627bb4
LeonE2024 <leonelflein@gmail.com>
2024-11-17 16:40:45 +0100

    v4l2 param sh

--- src/vpa_robot_vision/scripts/set_v4l2_params.sh
+++ src/vpa_robot_vision/scripts/set_v4l2_params.sh
@@ -3,5 +3,5 @@ sleep 1
 
 v4l2-ctl -d /dev/video0 --set-ctrl white_balance_auto_preset=3
 v4l2-ctl -d /dev/video0 --set-ctrl scene_mode=11
-v4l2-ctl -d /dev/video0 --set-ctrl contrast=0
-v4l2-ctl -d /dev/video0 --set-ctrl saturation=0
+# v4l2-ctl -d /dev/video0 --set-ctrl contrast=0
+# v4l2-ctl -d /dev/video0 --set-ctrl saturation=0
