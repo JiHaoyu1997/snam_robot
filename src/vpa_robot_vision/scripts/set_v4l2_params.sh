525050644c46baa3a71bcfe5380c7688827ad3b1
LeonE2024 <leonelflein@gmail.com>
2024-11-17 16:19:50 +0100

    v4l2 param sh

--- src/vpa_robot_vision/scripts/set_v4l2_params.sh
+++ src/vpa_robot_vision/scripts/set_v4l2_params.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 1
 
-v4l2-ctl -d /dev/video0 --set-ctrl white_balance_temprature_preset=7
+v4l2-ctl -d /dev/video0 --set-ctrl white_balance_tauto_preset=7
 v4l2-ctl -d /dev/video0 --set-ctrl scene_mode=11
 v4l2-ctl -d /dev/video0 --set-ctrl contrast=50
