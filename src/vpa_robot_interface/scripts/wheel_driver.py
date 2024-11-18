6d8f15fcefee23fe8090e281c3487230511687bd
LeonE2024 <leonelflein@gmail.com>
2024-11-18 19:07:20 +0100

    20241118 1845

--- src/vpa_robot_interface/scripts/wheel_driver.py
+++ src/vpa_robot_interface/scripts/wheel_driver.py
@@ -130,8 +130,8 @@ class WheelDriverNode:
             self.kp     = kp
             self.ki     = ki
         else:
-            self.kp     = 0.12
-            self.ki     = 0.015
+            self.kp     = 0.11
+            self.ki     = 0.012
 
         self.omega_controller_left  = PIController(ki=self.ki,kp=self.kp)
         self.omega_controller_right = PIController(ki=self.ki,kp=self.kp)
