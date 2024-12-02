1e24c6c13a62c4f9deafd42bdc523945a5a98d5a
LeonE2024 <leonelflein@gmail.com>
2024-12-02 17:23:42 +0100

    preset7

--- vpa_robot/scripts/visual_detector.py
+++ vpa_robot/scripts/visual_detector.py
@@ -494,15 +494,15 @@ class RobotVision:
         return 
 
     def go_thur_lane(self, cv_img, cv_hsv_img):
-        dis2red = search_pattern.search_line(hsv_image=cv_hsv_img, hsv_space=self.stop_line_hsv)
-        if dis2red > 30:
-            self.stop = True
-            rospy.loginfo("STOP")
-
         dis2green = search_pattern.search_line(hsv_image=cv_hsv_img, hsv_space=self.inter_boundary_line_hsv)
         if dis2green > 30:
             self.stop = False
             rospy.loginfo("Start")
+
+        dis2red = search_pattern.search_line(hsv_image=cv_hsv_img, hsv_space=self.stop_line_hsv)
+        if dis2red > 30 and self.stop_timer is None:
+            rospy.loginfo("STOP")
+            self.stop_timer = rospy.Timer(rospy.Duration(1), self.stop_cb, oneshot=True)
 
         target_x, _ = self.find_target_to_cross_lane(cv_img=cv_img, cv_hsv_img=cv_hsv_img)
         v_x, omega_z = self.calculate_velocity(target_x=target_x)
@@ -530,7 +530,7 @@ class RobotVision:
 
         dis2green = search_pattern.search_line(hsv_image=cv_hsv_img, hsv_space=self.inter_boundary_line_hsv)
         if dis2green > 30 and self.stop_timer is None:
-            rospy.loginfo("Start")
+            rospy.loginfo("STOP")
             self.stop_timer = rospy.Timer(rospy.Duration(1), self.stop_cb, oneshot=True)
        
         target_x, _ = self.find_target_to_cross_conflict(cv_img=cv_img, cv_hsv_img=cv_hsv_img, action=action)
