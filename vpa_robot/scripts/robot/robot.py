f290063cd98463d1f52550a344b7e7abf4f04d79
LeonE2024 <leonelflein@gmail.com>
2024-12-03 16:24:33 +0100

    vel calc

--- vpa_robot/scripts/robot/robot.py
+++ vpa_robot/scripts/robot/robot.py
@@ -49,10 +49,9 @@ class RobotMotion:
             pre_position = (self.prev_pose_data[1], self.prev_pose_data[2])
             distance = self.calculate_distance(pre_position, curr_position)
             time = self.curr_pose_data[0] - self.prev_pose_data[0]
-            vel = distance / time
-            print(vel)
             self.total_travel_distance += distance
-            rospy.loginfo(f"Current Travel Distance: {self.total_travel_distance:.3f}")
+            vel = distance / time
+            rospy.loginfo(f"Current Travel Distance: {self.total_travel_distance:.3f}, Current Velocity: {vel:.3f}")
 
         return
 
@@ -60,8 +59,8 @@ class RobotMotion:
     def calculate_distance(point1, point2):
         threshold = 0.005
         distance = math.sqrt((point2[0] - point1[0])**2 + (point2[1] - point1[1])**2)
-        if distance <= threshold:
-            return 0
+        # if distance <= threshold:
+        #     return 0
         return distance
     
     def vel_caculator(self, msg):
