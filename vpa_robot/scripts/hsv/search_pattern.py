99ef1bcc81215343fa8e6bfe5106238b92bdc4e1
LeonE2024 <leonelflein@gmail.com>
2024-12-06 16:06:03 +0100

    acc func

--- vpa_robot/scripts/hsv/search_pattern.py
+++ vpa_robot/scripts/hsv/search_pattern.py
@@ -121,7 +121,7 @@ def search_lane_center(space1:HSVSpace, space2:HSVSpace, hsv_image, is_yellow_le
     hsv_image2 = hsv_image
     mask1 = space1.apply_mask(hsv_image1)
     mask2 = space2.apply_mask(hsv_image2)
-    _line_center1 = _search_lane_linecenter(mask1, 30, -30, -10, int(hsv_image.shape[0]/2), 0, int(hsv_image.shape[1]))
+    _line_center1 = _search_lane_linecenter(mask1, -20, 30, 10, int(hsv_image.shape[0]/2), 0, int(hsv_image.shape[1]))
 
     if _line_center1 == 0 and not is_yellow_left:
         # failed to find the center yellow line
@@ -129,9 +129,9 @@ def search_lane_center(space1:HSVSpace, space2:HSVSpace, hsv_image, is_yellow_le
         
     # search the while line, but limited area
     if is_yellow_left:
-        _line_center2 = _search_lane_linecenter(mask2, 30, -30, -10, int(hsv_image.shape[0]/2), _line_center1, int(hsv_image.shape[1]), False)
+        _line_center2 = _search_lane_linecenter(mask2, -20, 30, 10, int(hsv_image.shape[0]/2), _line_center1, int(hsv_image.shape[1]), False)
     else:
-        _line_center2 = _search_lane_linecenter(mask2, 30, -30, -10, int(hsv_image.shape[0]/2), 0 ,_line_center1, False)
+        _line_center2 = _search_lane_linecenter(mask2, -20, 30, 10, int(hsv_image.shape[0]/2), 0 ,_line_center1, False)
 
     if is_yellow_left :
         # miss detections 
