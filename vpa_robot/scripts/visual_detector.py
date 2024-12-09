0a345b4de709fd609c0ca94a5415389a6e0bf4fd
LeonE2024 <leonelflein@gmail.com>
2024-12-09 15:35:56 +0100

    print counter line counter

--- vpa_robot/scripts/visual_detector.py
+++ vpa_robot/scripts/visual_detector.py
@@ -264,6 +264,7 @@ class RobotVision:
 
         if cross_inter_boundary:
             self.cross_inter_boundary_line_count += 1
+            print(self.cross_inter_boundary_line_count)
             if self.cross_inter_boundary_line_count >= 2:
                 if self.inter_boundary_detect_lock.acquire(blocking=False):
                     try:
