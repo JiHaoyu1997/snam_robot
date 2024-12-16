dec476bcd846250551f8288e6bc8c95ff327e6c0
LeonE2024 <leonelflein@gmail.com>
2024-12-16 11:22:53 +0100

    change green hsv

--- vpa_robot/scripts/hsv/hsv.py
+++ vpa_robot/scripts/hsv/hsv.py
@@ -35,7 +35,7 @@ HSV_RANGES = {
     'red_acc':  HSVSpace(h_u=180, h_l=175, s_u=255, s_l=150, v_u=255, v_l=60),
     'orange':   HSVSpace(h_u=30,  h_l=5,   s_u=255, s_l=90,  v_u=235, v_l=100),
     'yellow':   HSVSpace(h_u=36,  h_l=26,  s_u=255, s_l=100, v_u=255, v_l=100),
-    'green':    HSVSpace(h_u=95,  h_l=65,  s_u=255, s_l=100, v_u=255, v_l=75),
+    'green':    HSVSpace(h_u=95,  h_l=65,  s_u=255, s_l=100, v_u=255, v_l=100),
     'blue':     HSVSpace(h_u=120, h_l=105, s_u=255, s_l=100, v_u=255, v_l=100),
     'purple':   HSVSpace(h_u=170, h_l=140, s_u=255, s_l=100, v_u=255, v_l=100),
     'pink':     HSVSpace(h_u=175, h_l=145, s_u=255, s_l=80,  v_u=255, v_l=100),
