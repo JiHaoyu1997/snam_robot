f7c3c7065206a8d7a341f1da56b328e6fe2b1e96
LeonE2024 <leonelflein@gmail.com>
2024-12-17 18:31:22 +0100

    print length of seg

--- vpa_robot/scripts/visual_detector.py
+++ vpa_robot/scripts/visual_detector.py
@@ -468,6 +468,7 @@ class RobotVision:
         if target_x == None:
             if action == 2:
                 target_x = self.image_width * 0.7
+                print(0.7)
             else:
                 target_x = self.image_width / 2
         cv2.circle(cv_img, (int(target_x), int(cv_hsv_img.shape[0]/2)), 5, (255, 255, 0), 5)
