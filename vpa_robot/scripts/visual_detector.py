7f5c8eb4652f6d7e2d467728645e3ede97553c7e
LeonE2024 <leonelflein@gmail.com>
2024-12-19 18:39:38 +0100

    pub stop hsv iomg

--- vpa_robot/scripts/visual_detector.py
+++ vpa_robot/scripts/visual_detector.py
@@ -254,7 +254,9 @@ class RobotVision:
         self.pub_cmd_vel_from_img(v_x, omega_z, v_factor)
 
         # Step9 PUB IMAGE MESSAGES 
-        mask_img = self.buffer_line_hsv.apply_mask(hsv_image=cv_hsv_img)  
+        mask_img1 = self.stop_line_hsv.apply_mask(hsv_image=cv_hsv_img)  
+        mask_img2 = self.stop_line_hsv2.apply_mask(hsv_image=cv_hsv_img)  
+        mask_img = mask_img1 + mask_img2
         self.pub_cv_img(cv_img=result_cv_img)
         self.pub_acc_img(acc_img=acc_img)
         self.pub_mask_img(mask_img=mask_img)
