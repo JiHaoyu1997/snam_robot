7a546c0448cc7fd487f19b25834d490409fed7e9
LeonE2024 <leonelflein@gmail.com>
2025-01-07 17:05:21 +0100

    pub left

--- vpa_robot/scripts/visual_detector.py
+++ vpa_robot/scripts/visual_detector.py
@@ -260,7 +260,7 @@ class RobotVision:
         self.pub_cmd_vel_from_img(v_x, omega_z, v_factor)
 
         # Step10 PUB IMAGE MESSAGES 
-        mask_img = self.right_guide_hsv.apply_mask(hsv_image=cv_hsv_img) 
+        mask_img = self.left_guide_hsv.apply_mask(hsv_image=cv_hsv_img) 
         self.pub_mask_img(mask_img=mask_img)
         self.pub_cv_img(cv_img=result_cv_img)
         # self.pub_acc_img(acc_img=acc_img)
